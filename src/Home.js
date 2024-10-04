50981c59a313ca87c1f636ecb5c1d98a01f4431a
Janet Meng <jm914@duke.edu>
2024-10-04 01:45:28 -0400

    Created frontend: button, user input, and display

---
+++ src/Home.js
@@ -0,0 +1,37 @@
+import {useState} from 'react';
+
+const Home = () => {
+    const [notes, setNotes] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleClick = () => {
+        if (inputValue.trim()) { // for non empty input
+            setNotes([...notes, inputValue]); // add note to notes[]
+            setInputValue(''); // clear the input field
+        }
+    }
+
+    return ( 
+        <div className="home">
+            <p> Notes: </p>
+            <ul>
+                {notes.map((note, index) => (
+                    <li key={index}>{note}</li> // display each note in a list
+                ))}
+            </ul>
+            <input 
+                type="text" 
+                value={inputValue} 
+                onChange={handleInputChange} 
+                placeholder="Write a new note" 
+            />
+            <button onClick={handleClick}> Add the note </button>
+        </div>
+     );
+}
+ 
+export default Home;
